b18a176ccbcb49d81118fa2936cc6d5901a0bfbc
Raphael Scursoni <ra.scursoni@gmail.com>
2020-12-15 10:39:47 -0300

    Manipulando e aplicando conceito de enums

---
+++ Enums/Program.cs
@@ -0,0 +1,30 @@
+﻿using Enums.Entities;
+using System;
+using Enums.Entities.Enums;
+
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+
+            Console.WriteLine(os);
+            Console.WriteLine(txt);
+        }
+    }
+}
